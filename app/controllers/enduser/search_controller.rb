46c79fe2f9757c4880a4485f9b6e52bc93306cde
EC2 Default User <ec2-user@ip-172-31-27-188.ap-northeast-1.compute.internal>
2020-12-06 12:11:37 +0000

    [12-06]改：絞り込み検索

--- app/controllers/enduser/search_controller.rb
+++ app/controllers/enduser/search_controller.rb
@@ -1,46 +1,30 @@
 class Enduser::SearchController < Enduser::Base
 
   def search
-    @word = params[:search]
+    @words = params[:search]
+    @pets = Pet.all
 
-    if params[:search].present?
-      @pets = Pet.where('name like ? or kind like ?',"%#{@word}%","%#{@word}%")
-    end
+    @pets = @pets.where('name like ? or kind like ?',"%#{@words}%","%#{@words}%") if @words.present?
 
-    if params[:age].present?
-      @pets = Pet.where("age like ?", params[:age])
-    end
+    @pets = @pets.where("age like ?", Pet.ages[params[:age]]) if params[:age].present? && params[:age] != 'blank'
 
-    if params[:sex].present?
-      @pets = Pet.where("sex like ?", params[:sex])
-    end
+    @pets = @pets.where("sex like ?", Pet.sexes[params[:sex]]) if params[:sex].present? && params[:sex] != 'blank'
 
-    if params[:size].present?
-      @pets = Pet.where("size like ?", params[:size])
-    end
+    @pets = @pets.where("size like ?", Pet.sizes[params[:size]]) if params[:size].present? && params[:size] != 'blank'
 
-    if params[:vaccine_status].present?
-      @pets = Pet.where("vaccine_status like ?", params[:vaccine_status])
-    end
+    @pets = @pets.where("vaccine_status like ?", Pet.vaccine_statuses[params[:vaccine_status]]) if params[:vaccine_status].present? && params[:vaccine_status] != 'blank'
 
-    if params[:castration_status].present?
-      @pets = Pet.where("castration_status like ?", params[:castration_status])
-    end
+    @pets = @pets.where("castration_status like ?", Pet.castration_statuses[params[:castration_status]]) if params[:castration_status].present? && params[:castration_status] != 'blank'
 
-    if params[:single_status].present?
-      @pets = Pet.where("single_status like ?", params[:single_status])
-    end
+    @pets = @pets.where("single_status like ?", Pet.single_statuses[params[:single_status]]) if params[:single_status].present? && params[:single_status] != 'blank'
 
-    if params[:older_status].present?
-      @pets = Pet.where("older_status like ?", params[:older_status])
-    end
+    @pets = @pets.where("older_status like ?", Pet.older_statuses[params[:older_status]]) if params[:older_status].present? && params[:older_status] != 'blank'
 
-    if params[:prefectures].present?
-      @pets = Pet.where("prefectures like ?", params[:prefectures])
-    end
+    @pets = @pets.where("prefectures like ?", Pet.prefectures[params[:prefectures]]) if params[:prefectures].present? && params[:prefectures] != 'blank'
 
 
-    @pets = @pets
+    @words = []
+    @words << params[:search] << Pet.ages_i18n[params[:age]] << Pet.sexes_i18n[params[:sex]] << Pet.sizes_i18n[params[:size]] << Pet.vaccine_statuses_i18n[params[:vaccine_status]] << Pet.castration_statuses_i18n[params[:castration_status]] << Pet.single_statuses_i18n[params[:single_status]] << Pet.older_statuses_i18n[params[:older_status]] << Pet.prefectures_i18n[params[:prefectures]]
+    @words.delete('--入力してください--')
   end
-#@pets = Pet.where('name like ? or kind like ? or sex like ? or age like ? or size like ?',"%#{@word}%","%#{@word}%","%#{@word}%","%#{@word}%","%#{@word}%")
 end
