b39c0ee343267211a2256dcc8c6777a6d431f358
EC2 Default User <ec2-user@ip-172-31-27-188.ap-northeast-1.compute.internal>
2020-12-07 11:22:00 +0000

    [12-07]validate追加

--- app/models/reservation.rb
+++ app/models/reservation.rb
@@ -1,7 +1,7 @@
 class Reservation < ApplicationRecord
   validates :start_date, {presence: true, uniqueness: true}
-  validate :start_check
-  validate :start_cannot
+  validate :start_check, if: :start_date
+  validate :start_cannot, if: :start_date
 
 
   # 開始時間と現在日時の比較
@@ -9,7 +9,7 @@ class Reservation < ApplicationRecord
     errors.add(:start_date, "は現在の時刻より前に時間になっています") if self.start_date < Time.now
   end
 
-  # 開始時間と指定日時の比較
+  # 開始時間を(10:00〜18:00)
   def start_cannot
     errors.add(:start_date, "は営業時間外です") if  Time.parse(self.start_date.strftime("%Y/%m/%d %H:%M")).hour < 10 || Time.parse(self.start_date.strftime("%Y/%m/%d %H:%M")).hour > 18
   end
